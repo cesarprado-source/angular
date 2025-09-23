a0d3923f0e510beae377b143b3b10d3a61a6f414
Cesar Hector Prado Vallejo <cesar.prado@endesyc.bo>
2025-09-22 20:53:44 -0400

    feat(home): integrate template layout into Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Ui } from './pages/ui/ui';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home }, 
+    { path: 'ui', component: Ui },
+];
